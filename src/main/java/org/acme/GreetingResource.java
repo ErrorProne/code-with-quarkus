bfa6b58943d247f8f15890d5848e378d3283901d
fherpich <finn@thebuilders.de>
2023-11-20 15:01:05 +0100

    push reproducer

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,16 +1,44 @@
 package org.acme;
 
+import io.quarkus.vertx.ConsumeEvent;
+import io.vertx.core.eventbus.EventBus;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("/")
 public class GreetingResource {
 
+    @Inject
+    EventBus eventBus;
+
     @GET
+    @Path("/blocking")
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from RESTEasy Reactive";
+    public void blocking()  {
+        eventBus.send("blocking", "eventbus call");
+    }
+
+    @GET
+    @Path("/nonblocking")
+    @Produces(MediaType.TEXT_PLAIN)
+    public void nonblocking()  {
+        eventBus.send("nonBlocking", "eventbus call");
+    }
+
+    @ConsumeEvent(value = "blocking", blocking = true)
+    public void blockingException(String testMessage) {
+        if (1==1) {
+            throw new RuntimeException("I SHOULD BE IN THE LOG");
+        }
+    }
+
+    @ConsumeEvent(value = "nonBlocking")
+    public void nonblockingException(String testMessage) {
+        if (1==1) {
+            throw new RuntimeException("I SHOULD BE IN THE LOG");
+        }
     }
 }
--- src/test/java/org/acme/GreetingResourceTest.java
+++ src/test/java/org/acme/GreetingResourceTest.java
@@ -10,12 +10,15 @@ import static org.hamcrest.CoreMatchers.is;
 public class GreetingResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
+    public void nonBlockingConsume() {
         given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("Hello from RESTEasy Reactive"));
+          .when().get("/nonblocking");
+    }
+
+    @Test
+    public void blockingConsume() {
+        given()
+                .when().get("/blocking");
     }
 
 }
